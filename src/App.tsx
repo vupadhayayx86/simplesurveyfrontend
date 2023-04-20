62d64089595e06aee53ed6ab7f2e7aa6f31aede1
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-04-20 16:53:51 +0530

    Frontend Final

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SurveyForm from "./components/SurveyForm";
+
+const App=()=>{
+  return(
+    <div className="container p-5">
+      <h1 className="mb-3"><u>Registration Form</u></h1>
+      <SurveyForm />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/SurveyForm.tsx
@@ -0,0 +1,68 @@
+import React, { FormEvent } from 'react'
+import {useForm,SubmitHandler} from "react-hook-form"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Props{
+    username:String,
+    email:String,
+    password:String,
+    confirmpass:String
+}
+
+const SurveyForm = () => {
+    const {register,handleSubmit,formState:{errors},reset} =useForm<Props>()
+  
+
+    const onFormSubmit=async (data:Props)=>{
+      console.log(data)
+      if(data.password!==data.confirmpass){
+        toast("Password do not match")
+        return
+      }
+      
+      const response=await fetch("https://backendsurvey.onrender.com/users",{
+        method:'POST',
+        body:JSON.stringify(data),
+        headers:{
+          'Content-Type':'application/json'
+        }
+      })
+
+      const json=await response.json()
+      if(!response.ok){
+        console.log("Something went wrong in form submission",json)
+      }
+      if(response.ok){
+        console.log("User created Successfully",json)
+        reset()
+      }
+    }
+
+    const resetForm=(e:FormEvent)=>{
+      e.preventDefault()
+      reset()
+    }
+
+  return (
+    <form onSubmit={handleSubmit((data)=>onFormSubmit(data))}>
+        <label htmlFor="username">Username</label>
+        <input type="text" className='form-control w-25' id='username' {...register("username",{required:true})} />
+        {errors.username && <p className='text-danger'>Field cannot be empty.</p>}
+        <label htmlFor="email">Email</label>
+        <input type="email" className='form-control w-25' id='email' {...register("email", {required:true})} />
+        {errors.email && <p className='text-danger'>Field cannot be empty.</p>}
+        <label htmlFor="password">Password</label>
+        <input type="password" className='form-control w-25' id='password' {...register("password",{required:true})} />
+        {errors.password && <p className='text-danger'>Field cannot be empty.</p>}
+        <label htmlFor="confirmpass">Confirm Password</label>
+        <input type="password" id="confirmpass" className='form-control w-25' {...register("confirmpass",{required:true})} />
+        {errors.confirmpass && <p className='text-danger'>Field cannot be empty.</p>}
+        <ToastContainer />
+        <button className="btn btn-primary" type='submit'>Submit</button>
+        <button className="btn btn-danger m-3" onClick={(e)=>resetForm(e)}>Clear</button>
+    </form>
+  )
+}
+
+export default SurveyForm
